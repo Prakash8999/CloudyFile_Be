f92d66f1b3a6fdfd913361c800a8a92a21d97794
Prakash8999 <jhaprakash504@gmail.com>
2025-07-08 23:06:06 +0530

    made changes

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(helmet())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','http://localhost:4173'],
   // methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
